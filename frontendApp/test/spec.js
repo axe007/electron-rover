0e7db3540e05aa3925626959f53db3036ac7366e
Altansukh T <altansukh@altansukh.com>
2021-04-19 03:17:58 +0800

    Update spec.js

--- frontendApp/test/spec.js
+++ frontendApp/test/spec.js
@@ -2,39 +2,56 @@ const Application = require('spectron').Application
 const assert = require('assert')
 const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
 const path = require('path')
+const chaiAsPromised = require("chai-as-promised");
+const chai = require("chai");
 
-describe('Application launch', function () {
+chai.should();
+chai.use(chaiAsPromised);
+
+describe("Application launch test", function () {
   this.timeout(10000)
-
-  beforeEach(function () {
-    this.app = new Application({
-      // Your electron path can be any binary
-      // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
-      // But for the sake of the example we fetch it from our node_modules.
-      path: electronPath,
-      args: [path.join(__dirname, '..')]
-    })
-    return this.app.start()
-  })
-
-  afterEach(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop()
-    }
-  })
-
-  it('Shows an initial window', function () {
-    return this.app.client.getWindowCount().then(function (count) {
-      assert.equal(count, 1)
-      // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-      // assert.equal(count, 2)
-    })
-  })
 	
-
-	it('Displays a title', function () {
-		return this.app.client.browserWindow.getTitle().then(function (title) {
-			assert.equal(title, 'SmartRover - Home')
-		})
+	beforeEach(function () {
+		
+   		const appPath = path.join(__dirname, "../");
+		
+		this.app = new Application({
+			path: electronPath,
+			args: [appPath],
+			env: {
+				ELECTRON_ENABLE_LOGGING: true,
+				ELECTRON_ENABLE_STACK_DUMPING: true,
+				NODE_ENV: "development",
+				PORT: 9515
+			},
+			chromeDriverArgs: ['--headless','--no-sandbox','--disable-dev-shm-usage'],
+			startTimeout: 20000,
+			chromeDriverLogPath: '../chromedriverlog.txt'
+  		});
+		
+		chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
+		return this.app.start();      
 	});
-})
+
+ 	afterEach(function () {
+		if (this.app && this.app.isRunning()) {
+			return this.app.stop();
+		}
+	});
+
+	describe("Launch", function () {
+		it('Shows an initial window', function () {
+			return this.app.client.getWindowCount().then(function (count) {
+				assert.equal(count, 1)
+				// Please note that getWindowCount() will return 2 if `dev tools` are opened.
+				// assert.equal(count, 2)
+			})
+		})
+
+		it('Displays a title', function () {
+			return this.app.client.browserWindow.getTitle().then(function (title) {
+				assert.equal(title, 'SmartRover - Home')
+			})
+		});
+	});
+});
