a4579260434f6361a01982a5c4f93435c5e8473f
Altansukh T <altansukh@altansukh.com>
2021-04-18 18:54:35 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,74 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+
+const path = require('path');
+
+function createWindow () {
+	const mainWindow = new BrowserWindow({
+		width: 1280,
+		height: 960,
+		icon: path.join(__dirname,'./assets/images/favicon.ico'),
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			enableRemoteModule: true,
+			preload: path.join(__dirname, 'preload.js')
+		}
+	});
+
+	mainWindow.loadFile('index.html');
+	
+	/* Global shortcut for F5 to reload window */
+	globalShortcut.register('f5', function() {
+		mainWindow.reload();
+	});
+	
+	globalShortcut.register('CommandOrControl+R', function() {
+		mainWindow.reload();
+	});
+}
+
+app.whenReady().then(() => {
+	createWindow()
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('will-quit', () => {
+	// Unregister a shortcut.
+	globalShortcut.unregister('CommandOrControl+X')
+
+	// Unregister all shortcuts.
+	globalShortcut.unregisterAll()
+})
+
+//For header position
+// When the user scrolls the page, execute myFunction
+/*
+mainWindow.onscroll= function() {myFunction()};
+
+
+// Get the header
+const document= mainWindow.loadFile('manual-control.html')
+var header  = document.getElementById(myHeader);
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (mainWindow.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}*/
---
+++ src/test/spec.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const path = require('path');
+const Application = require('spectron').Application;
+const electronPath = require('electron');
+
+const app = new Application({
+  path: electronPath,
+  args: [path.join(__dirname, '../')]
+});
+
+describe('Electron app tests', function () {
+	
+	this.timeout(5000)
+
+	//Start the electron app before each test
+	beforeEach(() => {
+		return app.start();
+	});
+
+	//Stop the electron app after completion of each test
+	afterEach(() => {
+		if (app && app.isRunning()) {
+			return app.stop();
+		}
+	});
+
+	it('Display the electron app window', async () => {
+		const count = await app.client.getWindowCount();
+		return assert.equal(count, 1);
+	});
+
+	it('Displays a title', async () => {
+		const title = await app.browserWindow.getTitle();
+		return assert.equal(title, 'SmartRover - Home');
+	});
+});
